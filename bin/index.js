c5fb2aec5fa6403bed2ecd0473c176ff2da55c21
Alex Merced <alex@alexmerced.dev>
2022-12-30 11:22:51 -0500

    first release

---
+++ bin/index.js
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+import { log } from "mercedlogger";
+import { execSync } from "child_process";
+
+const [, , operation, arg1] = process.argv;
+
+switch (operation) {
+  case "newbasicproject":
+    log.cyan("Process", "Generating New CoquitoJS Project");
+    execSync(`npx degit@latest AlexMercedCoder/CoquitoJS-Template ${arg1}`);
+    log.cyan(
+      "Process",
+      `Project generated, cd into ${
+        arg1 ? arg1 : "my-app"
+      } folder and run npm install`
+    );
+    break;
+
+  case "scaffold":
+    break;
+
+  case "generate":
+    break;
+
+  default:
+    console.log(`
+        -----------------------------------------------------------
+                        No Operation Specified
+            OPTIONS:
+            - newbasicproject [projectname] (create new coquito app from basic template)
+            - scaffold [path to scaffold.json]
+            - generate [item to generate] [name]
+        -----------------------------------------------------------
+        `);
+}
